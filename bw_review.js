e4e4ca1bf163c910c99b3958c1e9e4de69809dcd
Connor Webdale <CWebdale006@west-mec.org>
2019-03-15 13:28:12 -0700

    Everything works
    Changed "mouseclick" to "click" and it worked, wow
    Wrote functions to keep track of and display the total amount of characters in a text box

--- bw_review.js
+++ bw_review.js
@@ -77,24 +77,48 @@ function lightStars() {
       event.target.addEventListener("mouseleave", turnOffStars);
 
       // Moving the mouse pointer off the star should not remove the rating. Adds an event listener for the target of the event object that runs an anonymous function removing the turnOffStars() function. 
-      event.target.addEventListener("mouseclick", function () {
+      event.target.addEventListener("click", function () {
             event.target.removeEventListener("mouseleave", turnOffStars);
       })
 }
 
 function turnOffStars() {
+      // The stars variable contains the object collection referenced by the selector "span#stars img"
       var stars = document.querySelectorAll("span#stars img");
 
+      // Changes the src attribute of each image to the original star. 
       for (var i = 0; i < stars.length; i++) {
             stars[i].src = "bw_star.png";
       }
+
+      // Changes the value of the rating input box to an empty text string 
+      document.getElementById("rating").value = "";
 }
 
+// Keeps a running total of the number of characters that the user has typed into the comment text area box. 
+function updateCount() {
+      // Sets commentText equal to the value stored in the comment text area box. 
+      var commentText = document.getElementById("comment").value;
 
+      // Use the countCharacters() function with commentText as the parameter value to calculate the number of characters in commentText. 
+      var charCount = countCharacters(commentText);
 
+      // Declares "wordCountBox" that references the wordCount input box. 
+      var wordCountBox = document.getElementById("wordCount").value;
 
+      // Changes the value stored in the wordCount input box to a text string displaying the amount of characters out of 1000. 
+      document.getElementById("wordCount").value = charCount + "/1000";
 
+      // If charChount is greater than 1000, changes the color and background color, otherwise set the background white and font to black. 
+      if (charCount > 1000) {
+            document.getElementById("wordCount").style.color = "white";
+            document.getElementById("wordCount").style.backgroundColor = "red";
+      } else {
+            document.getElementById("wordCount").style.color = "black";
+            document.getElementById("wordCount").style.backgroundColor = "white";
+      }
 
+}
 
 
 /*=================================================================*/
